00d27c060484d4a8745cc573a6d9c172512913e9
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-08-23 17:23:03 +0700

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Intro } from "@/components";
+import { About, Intro, Project, SectionDivider } from "@/components";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <SectionDivider />
+      <About />
+      <Project />
     </main>
   );
 }
--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,7 @@
-import Header from "./Header";
-import Intro from "./Intro";
+import About from "./about";
+import Header from "./header";
+import Intro from "./intro";
+import Project from "./project";
+import SectionDivider from "./section-header";
 
-export { Header, Intro };
+export { Header, Intro, SectionDivider, About, Project };
