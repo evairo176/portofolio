b24c9a9e6127a45e0af4184ed9ff5fc94e455235
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-09-12 19:47:57 +0700

    add: add image project mobile mode

--- components/project/index.tsx
+++ components/project/index.tsx
@@ -47,6 +47,23 @@ const ProjectComponent = ({
       id="projects"
     >
       <section className="bg-gray-100 max-w-[42rem] border borderBlack rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <Image
+          src={imageUrl}
+          alt="Project I worked on"
+          quality={95}
+          className="relative  block sm:hidden w-[100%] max-h-[300px] object-cover object-center rounded-t-lg shadow-2xl
+      transition 
+      group-hover:scale-[1.04]
+      group-hover:-translate-x-3
+      group-hover:translate-y-3
+      group-hover:-rotate-2
+
+      group-even:group-hover:translate-x-3
+      group-even:group-hover:translate-y-3
+      group-even:group-hover:rotate-2
+
+      "
+        />
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2  sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <Link href={link}>
             <h3 className="text-2xl font-semibold">{title}</h3>
